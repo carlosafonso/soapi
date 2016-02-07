6d4c70a8b88848ed6e6c3baaa49671323bf5edc2
Carlos Afonso Pérez <cafonso@e-valua.es>
2016-02-07 14:12:00 +0100

    Add dummy test to avoid Travis builds to fail.
    (Real tests should come pretty soon though.)

---
+++ tests/SoapiTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Afonso\Soapi\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+class SoapiTest extends PHPUnit_Framework_TestCase
+{
+    public function testSoapi()
+    {
+        /*
+         * Temporary placeholder test to avoid
+         * Travis builds to fail.
+         */
+    }
+}
