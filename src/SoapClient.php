f7370a2d2dca08d006edfd7ab908558d90b43a80
Carlos Afonso Pérez <cafonso@e-valua.es>
2016-02-07 15:03:38 +0100

    Tabs to spaces (adhere to PSR2)

--- src/SoapClient.php
+++ src/SoapClient.php
@@ -14,60 +14,60 @@ use SoapClient as NativeSoapClient;
  */
 class SoapClient extends NativeSoapClient
 {
-	use ProcessesWithPipelines;
+    use ProcessesWithPipelines;
 
-	/**
-	 * Creates and returns a new SoapClient
-	 * with empty inbound and outbound pipelines.
-	 *
-	 * @param	string|null	$wsdl
-	 * @param	array		$options
-	 * @return	SoapClient
-	 */
-	public function __construct($wsdl, array $options = array()/*, $autoresponse = true*/)
-	{
-		parent::__construct($wsdl, $options);
+    /**
+     * Creates and returns a new SoapClient
+     * with empty inbound and outbound pipelines.
+     *
+     * @param   string|null $wsdl
+     * @param   array       $options
+     * @return  SoapClient
+     */
+    public function __construct($wsdl, array $options = array()/*, $autoresponse = true*/)
+    {
+        parent::__construct($wsdl, $options);
 
-		// initialize in and outbound pipelines
-		$this->inboundPipeline = new Pipeline();
-		$this->outboundPipeline = new Pipeline();
-	}
+        // initialize in and outbound pipelines
+        $this->inboundPipeline = new Pipeline();
+        $this->outboundPipeline = new Pipeline();
+    }
 
-	/**
-	 * Performs a SOAP request.
-	 *
-	 * This function delegates on PHP's native
-	 * SoapClient __doRequest(), but processes the
-	 * payload through the oubtound and inbound pipelines.
-	 * This allows for arbitrary manipulation of
-	 * requests and responses, such as encryption or
-	 * logging.
-	 *
-	 * @see		http://php.net/manual/en/soapclient.dorequest.php
-	 * @param	string	$soapRequest
-	 * @param	string	$location
-	 * @param	string	$action
-	 * @param	int		$version
-	 * @param	int		$oneWay
-	 */
-	public function __doRequest($soapRequest, $location, $action, $version, $oneWay = 0)
-	{
-		/*
-		 * Run the request XML through the outbound
-		 * pipeline
-		 */
-		$soapRequest = $this->outboundPipeline->process($soapRequest);
+    /**
+     * Performs a SOAP request.
+     *
+     * This function delegates on PHP's native
+     * SoapClient __doRequest(), but processes the
+     * payload through the oubtound and inbound pipelines.
+     * This allows for arbitrary manipulation of
+     * requests and responses, such as encryption or
+     * logging.
+     *
+     * @see     http://php.net/manual/en/soapclient.dorequest.php
+     * @param   string  $soapRequest
+     * @param   string  $location
+     * @param   string  $action
+     * @param   int     $version
+     * @param   int     $oneWay
+     */
+    public function __doRequest($soapRequest, $location, $action, $version, $oneWay = 0)
+    {
+        /*
+         * Run the request XML through the outbound
+         * pipeline
+         */
+        $soapRequest = $this->outboundPipeline->process($soapRequest);
 
-		/*
-		 * Do the actual request
-		 */
-		$soapResponse = parent::__doRequest($soapRequest, $location, $action, $version, $oneWay);
+        /*
+         * Do the actual request
+         */
+        $soapResponse = parent::__doRequest($soapRequest, $location, $action, $version, $oneWay);
 
-		/*
-		 * Then run the response through the inbound
-		 * pipeline
-		 */
-		$soapResponse = $this->inboundPipeline->process($soapResponse);
-		return $soapResponse;
-	}
+        /*
+         * Then run the response through the inbound
+         * pipeline
+         */
+        $soapResponse = $this->inboundPipeline->process($soapResponse);
+        return $soapResponse;
+    }
 }
--- src/SoapServer.php
+++ src/SoapServer.php
@@ -14,82 +14,82 @@ use SoapServer as NativeSoapServer;
  */
 class SoapServer extends NativeSoapServer
 {
-	use ProcessesWithPipelines;
+    use ProcessesWithPipelines;
 
-	// protected $autoresponse = true;
+    // protected $autoresponse = true;
 
-	/**
-	 * Creates and returns a new SoapServer
-	 * with empty inbound and outbound pipelines.
-	 *
-	 * @param	string|null	$wsdl
-	 * @param	array		$options
-	 * @return	SoapServer
-	 */
-	public function __construct($wsdl, array $options = array()/*, $autoresponse = true*/)
-	{
-		parent::__construct($wsdl, $options);
-		// $this->autoresponse = $autoresponse;
+    /**
+     * Creates and returns a new SoapServer
+     * with empty inbound and outbound pipelines.
+     *
+     * @param   string|null $wsdl
+     * @param   array       $options
+     * @return  SoapServer
+     */
+    public function __construct($wsdl, array $options = array()/*, $autoresponse = true*/)
+    {
+        parent::__construct($wsdl, $options);
+        // $this->autoresponse = $autoresponse;
 
-		// initialize in and outbound pipelines
-		$this->inboundPipeline = new Pipeline();
-		$this->outboundPipeline = new Pipeline();
-	}
+        // initialize in and outbound pipelines
+        $this->inboundPipeline = new Pipeline();
+        $this->outboundPipeline = new Pipeline();
+    }
 
-	/**
-	 * Handles a SOAP request.
-	 *
-	 * This function delegates on PHP's native
-	 * SoapServer handle(), but processes the input
-	 * XML through the inbound and outbound pipelines.
-	 * This allows for arbitrary manipulation of
-	 * requests and responses, such as encryption or
-	 * logging.
-	 *
-	 * Just as the parent class, this function will
-	 * fetch the input from the POST data if none is
-	 * passed as an argument. Likewise, it will return
-	 * nothing and output the response instead.
-	 *
-	 * @see		http://php.net/manual/en/soapserver.handle.php
-	 * @param	string|null	$soapRequest
-	 */
-	public function handle($soapRequest = null)
-	{
-		if (! $soapRequest) {
-			$soapRequest = file_get_contents('php://input');
-		}
+    /**
+     * Handles a SOAP request.
+     *
+     * This function delegates on PHP's native
+     * SoapServer handle(), but processes the input
+     * XML through the inbound and outbound pipelines.
+     * This allows for arbitrary manipulation of
+     * requests and responses, such as encryption or
+     * logging.
+     *
+     * Just as the parent class, this function will
+     * fetch the input from the POST data if none is
+     * passed as an argument. Likewise, it will return
+     * nothing and output the response instead.
+     *
+     * @see     http://php.net/manual/en/soapserver.handle.php
+     * @param   string|null $soapRequest
+     */
+    public function handle($soapRequest = null)
+    {
+        if (! $soapRequest) {
+            $soapRequest = file_get_contents('php://input');
+        }
 
-		/*
-		 * Run the request XML through the inbound
-		 * pipeline
-		 */
-		$soapRequest = $this->inboundPipeline->process($soapRequest);
+        /*
+         * Run the request XML through the inbound
+         * pipeline
+         */
+        $soapRequest = $this->inboundPipeline->process($soapRequest);
 
-		/*
-		 * Execute the request
-		 */
-		ob_start();
-		parent::handle($soapRequest);
-		$soapResponse = ob_get_clean();
+        /*
+         * Execute the request
+         */
+        ob_start();
+        parent::handle($soapRequest);
+        $soapResponse = ob_get_clean();
 
-		/*
-		 * Then run the response through the outbound
-		 * pipeline
-		 */
-		$soapResponse = $this->outboundPipeline->process($soapResponse);
+        /*
+         * Then run the response through the outbound
+         * pipeline
+         */
+        $soapResponse = $this->outboundPipeline->process($soapResponse);
 
-		/*
-		 * Output the response and we're done
-		 */
-		$this->outputResponse($soapResponse);
-	}
+        /*
+         * Output the response and we're done
+         */
+        $this->outputResponse($soapResponse);
+    }
 
-	protected function outputResponse($soapResponse)
-	{
-		echo $soapResponse;
-		header('Content-Type: text/xml; charset=utf8');
-		header('Content-Length: ' . strlen($soapResponse));
-		exit(0);
-	}
+    protected function outputResponse($soapResponse)
+    {
+        echo $soapResponse;
+        header('Content-Type: text/xml; charset=utf8');
+        header('Content-Length: ' . strlen($soapResponse));
+        exit(0);
+    }
 }
